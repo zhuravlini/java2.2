563d9565f4e030e4e84331df4206c8dda0b64d83
Tatiana Zhuravleva <zhuravlini@gmail.com>
2022-10-13 18:59:19 +0300

    исправлен код

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 100; //начальная сумма
-        int b = 1576; //сумма пополнения
+        int accountAmount = 100; //начальная сумма
+        int topUpAmount = 1576; //сумма пополнения
         int bonus; // количество бонусов за пополнение
         int amount; // общая сумма с пополнением и бонусами
-        if (b < 1000) {
+        if (topUpAmount < 1000) {
             bonus = 0;
-        } else {bonus = b/100;}
-        if (b > 1000) {
-            amount = a + b + bonus;
         } else {
-            amount = a + b;
+            bonus = topUpAmount / 100;
+        }
+        if (topUpAmount > 1000) {
+            amount = accountAmount + topUpAmount + bonus;
+        } else {
+            amount = accountAmount + topUpAmount;
         }
         System.out.println("Вам начислено " + bonus + " бонусных рублей");
         System.out.println("Итого на счете " + amount + " рублей");
